4ffac3d648fc41b56f8d74bfa87fa4676597553c
amgDVL01 <aurian.germain@edu.devinci.fr>
2025-12-15 15:36:23 +0100

    Add files via upload

---
+++ Plateau.cs
@@ -0,0 +1,18 @@
+﻿namespace Mots_glissés
+{
+    internal class Plateau
+    {
+        string[,] lettres;
+
+        public Plateau(string[,] l)
+        {
+            this.lettres = l;
+        }
+        public Plateau ()
+        {
+
+        }
+        public string[,] Lettres { get { return lettres; } }
+
+    }
+}
