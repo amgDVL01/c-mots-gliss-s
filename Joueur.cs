8b72b6004c2ccf7a06eb21a5836ccbdd43b4522d
amgDVL01 <aurian.germain@edu.devinci.fr>
2025-12-15 15:32:40 +0100

    Add files via upload

---
+++ Joueur.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+
+namespace Mots_glissés
+{
+    internal class Joueur //validé
+    {
+        string nom;
+        int score;
+        List<string> mots;
+        
+        public Joueur(string n)
+        {
+            this.nom = n;
+            this.score=0;
+            this.mots = new List<string>();
+        }
+
+        public string Nom { get { return nom; } }
+        public int Score { get { return score; } }
+        public List<string> Mots { get { return mots; } }
+
+        public void AddMot(string mot)
+        {
+            Mots.Add(mot);
+        }
+        public void AddScore(int val)
+        {
+            score += val;
+        }
+        public string toString()
+        {
+            string s = nom+"\nscore : "+score;
+            foreach (string mot in mots) s += "\n" + mot;
+            return s;
+
+        }
+        public bool Contient(string n)
+        {
+            return mots.Contains(n);
+        }
+    }
+}
