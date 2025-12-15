b6a5418c1bab7263b87c118d6de95fc3a437c768
amgDVL01 <aurian.germain@edu.devinci.fr>
2025-12-15 16:24:52 +0100

    dictionnaire v2 ne pas utiliser l'autre

--- Dictionnaire.cs
+++ Dictionnaire.cs
@@ -3,137 +3,85 @@ using System.IO;
 
 namespace Mots_glissés
 {
-    internal class Dictionnaire
+    internal class Dictionnaire //validé
     {
-        string [][] dico;
+        List<string> dico;
         public Dictionnaire() //validé
         {
-            string [] dico1 = File.ReadAllLines("Mots_Français.txt");
-            this.dico=new string[dico1.Length][];
-            foreach (string mots in dico1) this.dico[Array.IndexOf(dico1, mots)] = mots.Split(' ');
-        }
-        public string[] DicoLettre(string mot) //validé
-        {
-            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            return dico[alphabet.IndexOf(mot[0])];
-        }
-        //public string[] Tri_Fusion(string[] mots) //validé
-        //{
-        //    if (mots.Length <= 1) return mots;
-        //    int milieu = mots.Length / 2;
-        //    string[] gauche = new string[milieu];
-        //    string[] droite = new string[mots.Length - milieu];
-        //    Array.Copy(mots, 0, gauche, 0, milieu);
-        //    Array.Copy(mots, milieu, droite, 0, mots.Length - milieu);
-        //    gauche = Tri_Fusion(gauche);
-        //    droite = Tri_Fusion(droite);
-        //    return Fusion(gauche, droite);
-        //}
-        //static string[] Fusion(string[] gauche, string[] droite) //validé
-        //{
-        //    int i = 0;
-        //    int j = 0;
-        //    int k = 0;
-        //    string[] resultat = new string[gauche.Length + droite.Length];
-
-        //    while (i < gauche.Length && j < droite.Length)
-        //    {
-        //        if (gauche[i].Length <= droite[j].Length)
-        //        {
-        //            resultat[k] = gauche[i];
-        //            i++;
-        //        }
-        //        else
-        //        {
-        //            resultat[k] = droite[j];
-        //            j++;
-        //        }
-        //        k++;
-        //    }
-        //    while (i < gauche.Length)
-        //    {
-        //        resultat[k] = gauche[i];
-        //        i++;
-        //        k++;
-        //    }
-
-        //    while (j < droite.Length)
-        //    {
-        //        resultat[k] = droite[j];
-        //        j++;
-        //        k++;
-        //    }
-
-        //    return resultat;
-        //}
-
-        public string[] Tri_Fusion(string[] tableau)
-        {
-            if (tableau.Length <= 1)
-                return tableau;
-
-            int milieu = tableau.Length / 2;
-
-            string[] gauche = tableau.Take(milieu).ToArray();
-            string[] droite = tableau.Skip(milieu).ToArray();
-
-            gauche = Tri_Fusion(gauche);
-            droite = Tri_Fusion(droite);
-
-            return Fusion(gauche, droite);
-        }
-        public string[] Fusion(string[] gauche, string[] droite)
-        {
-            string[] resultat = new string[gauche.Length + droite.Length];
-            int i = 0, j = 0, k = 0;
-
-            while (i < gauche.Length && j < droite.Length)
+            this.dico = new List<string>();
+            string[] tabMots;
+            StreamReader sr = null;
+            try
             {
-                if (string.Compare(gauche[i], droite[j], StringComparison.Ordinal) <= 0)
+                sr = new StreamReader("Mots_Français.txt");
+                string line;
+                while ((line= sr.ReadLine()) != null)
                 {
-                    resultat[k++] = gauche[i++];
+                    tabMots = line.Split(' ');
+                    foreach (string mot in tabMots)
+                    {
+                        dico.Add(mot);
+                    }
                 }
-                else
+            }
+            catch (IOException e)
+            { 
+                Console.WriteLine(e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr!= null) sr.Close();
+            }
+            Tri_Fusion();
+        }
+        public List<string> Dico { get { return this.dico; } }
+        public void Tri_Fusion()
+        {
+           Tri_Fusion(0, dico.Count - 1);
+        }
+        public void Tri_Fusion(int gauche, int droite)
+        {
+            if (gauche < droite)
+            {
+                int pivot = Partition(gauche, droite);
+                Tri_Fusion(gauche, pivot - 1);
+                Tri_Fusion(pivot + 1, droite);
+            }
+        }
+        public int Partition(int gauche, int droite)
+        {
+            string pivot = dico[droite];
+            int i = gauche - 1;
+
+            for (int j = gauche; j < droite; j++)
+            {
+                if (ComparerMots(dico[j], pivot) < 0)
                 {
-                    resultat[k++] = droite[j++];
+                    i++;
+                    (dico[i], dico[j]) = (dico[j], dico[i]);
                 }
             }
 
-            while (i < gauche.Length)
-                resultat[k++] = gauche[i++];
-
-            while (j < droite.Length)
-                resultat[k++] = droite[j++];
-
-            return resultat;
+            (dico[i + 1], dico[droite]) = (dico[droite], dico[i + 1]);
+            return i + 1;
         }
-        //public bool RechercheDicho(string mot) // a verifier
-        //{
-        //    string[] MotsLettre = Tri_Fusion(DicoLettre(mot));
-        //    return RechercheDichoRecursif(mot, 0, MotsLettre.Length-1, MotsLettre);
-        //}
-        //public bool RechercheDichoRecursif(string mot, int debut, int fin, string[] mots) //a verifier
-        //{
-        //    if (debut > fin) return false;
-        //    int milieu = (debut + fin) / 2;
-        //    if (mot == mots[milieu]) return true;
-        //    if (mot.Length < mots[milieu].Length) return RechercheDichoRecursif(mot, debut, milieu-1, mots);
-        //    else return RechercheDichoRecursif(mot, milieu+1, fin, mots);
-        //}
         public bool RechercheDicho(string mot)
         {
-            string[] MotsLettre = Tri_Fusion(DicoLettre(mot));
-            return RechercheDichoRecursif(MotsLettre, mot, 0, MotsLettre.Length - 1);
+            return RechercheDichoRecursif(dico, mot, 0, dico.Count()-1);
         }
 
-        public bool RechercheDichoRecursif(string[] mots, string mot, int debut, int fin)
+        public bool RechercheDichoRecursif(List<string> mots, string mot, int debut, int fin)
         {
             if (debut > fin)
                 return false;
 
             int milieu = (debut + fin) / 2;
 
-            int comparaison = string.Compare(mot, mots[milieu], StringComparison.Ordinal);
+            int comparaison = ComparerMots(mot, mots[milieu]);
 
             if (comparaison == 0)
                 return true;
@@ -141,6 +89,24 @@ namespace Mots_glissés
                 return RechercheDichoRecursif(mots, mot, debut, milieu - 1);
             else
                 return RechercheDichoRecursif(mots, mot, milieu + 1, fin);
+        }
+        public int ComparerMots(string mot1, string mot2)
+        {
+            int res=0;
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            if (mot1.Length < mot2.Length) res=-1;
+            if (mot1.Length > mot2.Length) res= 1;
+            else
+            {
+                int i = 0, l=mot1.Length;
+                while (res==0 && i<l)
+                {
+                    if (alphabet.IndexOf(mot1[i]) < alphabet.IndexOf(mot2[i])) res = -1;
+                    if (alphabet.IndexOf(mot1[i]) > alphabet.IndexOf(mot2[i])) res = 1;
+                    i++;
+                }
+            }
+            return res;
         }
     }
 }
